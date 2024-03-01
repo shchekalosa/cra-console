b0d2c7a1a74b0e9c357e972197d2f95a656bd61f
shchekalosa <shchekalosa@gmail.com>
2024-03-01 00:29:01 -0800

    Added idea to gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+      //new code
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
